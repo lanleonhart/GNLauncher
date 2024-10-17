037d5a433145b8414cca290d76ddf8cb60b050c7
Rabid Baboon <jacob@rommann.com>
2024-10-17 08:31:27 -0700

    implemented logging support

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Serilog.Core;
+using Serilog;
 using System.Windows;
+using System.IO;
 
 namespace GNLauncher
 {
@@ -9,5 +10,17 @@ namespace GNLauncher
     /// </summary>
     public partial class App : Application
     {
+        public static readonly string TempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "gnwars_launcher");
+        static string LogFile => System.IO.Path.Combine(TempPath, "gnlauncher-log-.txt");
+        public static Logger Log;
+
+        public App()
+        {           
+            try { Directory.CreateDirectory(TempPath); }catch(System.Exception ex) { MessageBox.Show(ex.ToString()); }
+            Log = new LoggerConfiguration().WriteTo.File(LogFile, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            Log.Information("----------------------------------------------------------------");
+            Log.Information(DateTime.Now.ToString());
+        }
     }
 }
