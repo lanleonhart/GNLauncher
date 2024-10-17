f16c0d7c56467bf062e5d013382c9f9b27e0cc00
Rabid Baboon <jacob@rommann.com>
2024-10-17 08:33:03 -0700

    implementing download progress screen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,15 +1,8 @@
 ﻿using Microsoft.Win32;
 using System.IO;
-using System.Text;
+using System.Net.Http.Handlers;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace GNLauncher
 {
@@ -33,6 +26,7 @@ namespace GNLauncher
             if (File.Exists(openFile.FileName))
             {
                 textBox_exe_loc.Text = openFile.FileName; 
+                App.Log.Information($"BattleTech.exe location: {openFile.FileName}");
                 button_start_install.IsEnabled = true;
             }
             else
@@ -40,11 +34,44 @@ namespace GNLauncher
         }
 
         async private void button_start_install_Click(object sender, RoutedEventArgs e)
-        {            
+        {
+            App.Log.Information("Starting mod installation");
             step_select_exe.Visibility = Visibility.Hidden;
+            step_downloading.Visibility = Visibility.Visible;
+
             var files = new ModFiles();
-            await files.Download();
-            step_select_exe.Visibility = Visibility.Visible;            
+            await files.Download(UpdateDownloadProgress);
+
+            step_downloading.Visibility = Visibility.Hidden;
+            step_select_exe.Visibility = Visibility.Visible;
+            App.Log.Information("Finished mod installation");
+        }
+
+        private void button_open_log_Click(object sender, RoutedEventArgs e)
+        {            
+            System.Diagnostics.Process.Start(App.TempPath);
+        }
+
+        void UpdateDownloadProgress(int totalFiles, int filesDownloaded, string currentFile, HttpProgressEventArgs args)
+        {
+            step_downloading.Dispatcher.BeginInvoke(() =>
+            {
+                progressTotal.Maximum = totalFiles;
+                progressTotal.Value = filesDownloaded;
+
+                label_current_file.Content = $"Downloading {currentFile}";
+
+                if (args.TotalBytes.HasValue)
+                {
+                    progressCurrent.Maximum = args.TotalBytes.Value;
+                    progressCurrent.Value = args.BytesTransferred;
+                }
+                else
+                {
+                    progressCurrent.Maximum = 100;
+                    progressCurrent.Value = args.ProgressPercentage;
+                }
+            });
         }
     }
 }
