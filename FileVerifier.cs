98f966557b52c1eaeea4640ed82f511928dc2c0a
Rabid Baboon <jacob@rommann.com>
2024-11-21 07:54:00 -0800

    starting to implement file verfivation and restoration

---
+++ FileVerifier.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+
+namespace GNLauncher
+{
+    internal class FileVerifier
+    {
+        public string GenerateChecksum(string filePath)
+        {
+            using (FileStream stream = File.OpenRead(filePath))
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(stream);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToUpperInvariant();
+            }
+        }
+
+        public bool VerifyFileIntegrity(string filePath, string originalChecksum)
+        {
+            string currentChecksum = GenerateChecksum(filePath);
+            return currentChecksum.Equals(originalChecksum, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
